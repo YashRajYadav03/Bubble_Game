ec6553045cb429feaf9d7c92e4e8fb155bd25acd
YashRajYadav03 <yashrocks5678@gmail.com>
2023-10-25 02:31:17 +0530

    Added some more functionality and fixed some bugs

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var timer=60;
+var score=0;
+var hitrn=0;
+function increaseScore(){
+    score += 10;
+    document.querySelector("#score-value").textContent=score;
+}
 function getNewHit(){
-    var rn=Math.floor(Math.random()*10);
-    document.querySelector("#hit-value").textContent=rn;
+    hitrn=Math.floor(Math.random()*10);
+    document.querySelector("#hit-value").textContent=hitrn;
 }
 function makeBubble(){
 var x ="";
@@ -22,10 +28,22 @@ function runTimer(){
         }
         else{
             clearInterval(timerinterval);
+            document.querySelector("#p-bottom").innerHTML=`<h1>Game Over !! Your Score:  ${score}  </h1>
+            <br>
+            `;
         }
         
     },1000)
 }
+document.querySelector("#p-bottom").addEventListener("click",function(event){
+    var clickedNumber=Number(event.target.textContent);
+    if(clickedNumber === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+
+})
 runTimer();
 makeBubble();
 getNewHit();
